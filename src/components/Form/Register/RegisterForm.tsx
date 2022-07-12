76f12e59e4fec1236218da07b8e7e5158eb8cb6d
= <=>
2022-07-12 11:30:21 +0700

    f1

--- src/components/Form/Register/RegisterForm.tsx
+++ src/components/Form/Register/RegisterForm.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 import isEmpty from "validator/lib/isEmpty";
 import isEmail from "validator/lib/isEmail";
 import isStrongPassword from "validator/lib/isStrongPassword";
-import isLength from "validator/lib/isLength";
+
 import "../style.css";
 
 interface User {
@@ -14,11 +14,15 @@ interface User {
   password: string;
 }
 const RegisterForm: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([]);
+  const [isValid, setIsValid] = useState<boolean>(false);
 
-  const [name, setName] = useState<string>("");
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
+  const [users, setUsers] = useState<User[]>([]);
+  const [user, setUser] = useState<User>({
+    fullname: "",
+    email: "",
+    password: "",
+  });
+
   const [message, setMessage] = useState({
     nameMessage: "",
     emailMessage: "",
@@ -31,19 +35,19 @@ const RegisterForm: React.FC = () => {
 
   const onChangeName = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setMessage({ ...message, nameMessage: "" });
-    setName(e.target.value.trim());
+    setUser({ ...user, fullname: e.target.value.trim() });
     inputName.current?.parentElement?.classList.remove("invalid");
   };
 
   const onChangeEmail = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setMessage({ ...message, emailMessage: "" });
-    setEmail(e.target.value.trim());
+    setUser({ ...user, email: e.target.value.trim() });
     inputEmail.current?.parentElement?.classList.remove("invalid");
   };
 
   const onChangePassword = (e: React.ChangeEvent<HTMLInputElement>): void => {
     setMessage({ ...message, passwordMessage: "" });
-    setPassword(e.target.value.trim());
+    setUser({ ...user, password: e.target.value.trim() });
     inputPassword.current?.parentElement?.classList.remove("invalid");
   };
 
@@ -54,40 +58,42 @@ const RegisterForm: React.FC = () => {
       passwordMessage: "",
     };
 
-    if (isEmpty(name)) {
+    if (isEmpty(user.fullname)) {
       message.nameMessage = "Please input your name";
       inputName.current?.parentElement?.classList.add("invalid");
     }
-    if (isEmpty(email)) {
+    if (isEmpty(user.email)) {
       message.emailMessage = "Please input your email";
       inputEmail.current?.parentElement?.classList.add("invalid");
-    } else if (!isEmail(email)) {
+    } else if (!isEmail(user.email)) {
       message.emailMessage = "Your email is incorrect";
       inputEmail.current?.parentElement?.classList.add("invalid");
     }
 
-    if (isEmpty(password)) {
+    if (isEmpty(user.password)) {
       message.passwordMessage = "Please input your password";
       inputPassword.current?.parentElement?.classList.add("invalid");
-    } else if (!isStrongPassword(password)) {
-      message.passwordMessage = "Password is too short";
+    } else if (!isStrongPassword(user.password)) {
+      message.passwordMessage = "Password incorrect";
       inputPassword.current?.parentElement?.classList.add("invalid");
     }
     setMessage(message);
   };
 
   const onSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
     validateAll();
-    if (name && isEmail(email) && password) {
-      setUsers([
-        ...users,
-        { fullname: name, email: email, password: password },
-      ]);
+    if (
+      user.fullname &&
+      isEmail(user.email) &&
+      isStrongPassword(user.password)
+    ) {
+      setUsers([...users, user]);
+      setIsValid(true);
     } else {
       console.log("somethings missing");
     }
     console.log(users);
-    e.preventDefault();
   };
 
   return (
@@ -97,80 +103,76 @@ const RegisterForm: React.FC = () => {
           <TiArrowBackOutline />
         </div>
       </Link>
-      <form className="form" id="form-1" onSubmit={(e) => onSubmit(e)}>
-        <h2 className="heading">Register</h2>
-        <div className="spacer"></div>
-        {/* Name */}
-        <div className={`form-group`}>
-          <label htmlFor="fullname" className="form-label">
-            Full Name
-          </label>
-          <input
-            ref={inputName}
-            id="fullname"
-            name="fullname"
-            type="text"
-            placeholder="Enter your full name"
-            className="form-control"
-            onChange={onChangeName}
-          />
-          <span className="form-message">{message.nameMessage}</span>
+      {isValid ? (
+        <div>
+          <h3>Successfully</h3>
+          <Link to="/login">
+            <button type="submit" className="form-submit">
+              Login Now
+            </button>
+          </Link>
         </div>
+      ) : (
+        <form className="form" id="form-1" onSubmit={(e) => onSubmit(e)}>
+          <h2 className="heading">Register</h2>
+          <div className="spacer"></div>
+          {/* Name */}
+          <div className={`form-group`}>
+            <label htmlFor="fullname" className="form-label">
+              Full Name
+            </label>
+            <input
+              ref={inputName}
+              id="fullname"
+              name="fullname"
+              type="text"
+              placeholder="Enter your full name"
+              className="form-control"
+              onChange={onChangeName}
+            />
+            <span className="form-message">{message.nameMessage}</span>
+          </div>
+          {/* Email */}
+          <div className={`form-group`}>
+            <label htmlFor="email" className="form-label">
+              Email
+            </label>
+            <input
+              ref={inputEmail}
+              id="email"
+              name="email"
+              type="text"
+              placeholder="Ex: email@domain.com"
+              className="form-control"
+              autoComplete="email"
+              onChange={onChangeEmail}
+            />
+            <span className="form-message">{message.emailMessage}</span>
+          </div>
+          {/* Password */}
+          <div className={`form-group`}>
+            <label htmlFor="password" className="form-label">
+              Password
+            </label>
+            <input
+              ref={inputPassword}
+              id="password"
+              name="password"
+              type="password"
+              placeholder="Enter your password"
+              className="form-control"
+              autoComplete="current-password"
+              onChange={onChangePassword}
+            />
+            <span className="form-message">{message.passwordMessage}</span>
+          </div>
 
-        {/* Email */}
-        <div className={`form-group`}>
-          <label htmlFor="email" className="form-label">
-            Email
-          </label>
-          <input
-            ref={inputEmail}
-            id="email"
-            name="email"
-            type="text"
-            placeholder="Ex: email@domain.com"
-            className="form-control"
-            autoComplete="email"
-            onChange={onChangeEmail}
-          />
-          <span className="form-message">{message.emailMessage}</span>
-        </div>
-        {/* Password */}
-        <div className={`form-group`}>
-          <label htmlFor="password" className="form-label">
-            Password
-          </label>
-          <input
-            ref={inputPassword}
-            id="password"
-            name="password"
-            type="password"
-            placeholder="Enter your password"
-            className="form-control"
-            autoComplete="current-password"
-            onChange={onChangePassword}
-          />
-          <span className="form-message">{message.passwordMessage}</span>
-        </div>
-
-        {/* <div className="form-group">
-          <label htmlFor="password_confirmation" className="form-label">
-            Confirm Password
-          </label>
-          <input
-            id="password_confirmation"
-            name="password_confirmation"
-            placeholder="Confirm Password"
-            type="password"
-            className="form-control"
-          />
-          <span className="form-message">{message.passwordMessage}</span>
-        </div> */}
-
-        {/* Submit */}
-        <button type="submit" className="form-submit">
-          Register
-        </button>
-      </form>
+          {/* Submit */}
+          <button type="submit" className="form-submit">
+            Register
+          </button>
+        </form>
+      )}
     </div>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -10,16 +10,20 @@ import "./index.css";
 import App from "./App";
 import RegisterForm from "./components/Form/Register/RegisterForm";
 import LoginForm from "./components/Form/Login/LoginForm";
+import { Counter } from "./components/Screen/Screen";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}></Route>
-      <Route path="/register" element={<RegisterForm />} />
-      <Route path="/login" element={<LoginForm />} />
-    </Routes>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/register" element={<RegisterForm />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/screen" element={<Counter />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
 );
